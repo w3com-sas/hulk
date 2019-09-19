62839260f00b60f3567174f30b4ee5a899db96b8
corentin-gillet <cgillet@w3com.fr>
2019-09-19 14:55:31 +0200

    Fix base of login

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -59,7 +59,7 @@ class AdminController extends AbstractController
     private function checkUser()
     {
         $user = $this->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
+        if (!is_object($user) || !$user instanceof UserInterface || !in_array('ROLE_ADMIN_HULK', $user->getRoles())) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
     }
