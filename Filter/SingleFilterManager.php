c32c6573490c8fc00a4b4ea7f63b3995289fabc4
corentin-gillet <cgillet@w3com.fr>
2019-10-10 20:24:46 +0200

    Add null value in filter

--- Filter/SingleFilterManager.php
+++ Filter/SingleFilterManager.php
@@ -42,6 +42,7 @@ class SingleFilterManager extends AbstractFilterManager
                     }
 
                     $values = [];
+                    $values[] = '';
                     foreach ($dataTable->getData() as $line) {
 
                         foreach ($line as $property => $value) {
