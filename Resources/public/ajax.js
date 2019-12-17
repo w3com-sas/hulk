4b7b8b086cee150f6f0fccc8cf910c8abf173f2c
corentin-gillet <cgillet@w3com.fr>
2019-12-17 09:50:12 +0100

    reloadDisplayForm

--- Resources/public/ajax.js
+++ Resources/public/ajax.js
@@ -1,4 +1,4 @@
-window.updateSapLine = function(input) {
+window.updateSapLine = function (input) {
     if (input.className.indexOf('is-valid') !== -1) {
         input.className = input.className.replace('is-valid', '');
     }
@@ -74,7 +74,7 @@ window.updateSap = function (data, targetEntity, targetField, entityKey, targetD
     })
 };
 
-window.apiRequest = function(data, params, urlApi, modal) {
+window.apiRequest = function (data, params, urlApi, modal) {
     var rows = this.formatDataToUpdate(data);
     var postData = {
         "data": rows,
@@ -170,36 +170,34 @@ window.reloadDisplayForm = function () {
         type: "POST",
         data: data,
         success: function (resp) {
-            if (resp.length > 0) {
-                var fieldsNotToUpdate = Object.keys(data.selectedChoices);
-                var toUpdateData = {};
-
-                // Organize data
-                for (var i = 0; i < resp.length; i++) {
-                    var properties = Object.keys(resp[i]);
-                    for (var l = 0; l < properties.length; l++) {
-                        if (!fieldsNotToUpdate.includes(properties[l])) {
-                            if (!toUpdateData.hasOwnProperty(properties[l])) {
-                                toUpdateData[properties[l]] = [];
-                            } else {
-                                if (!toUpdateData[properties[l]].includes(resp[i][properties[l]]) && resp[i][properties[l]] !== null) {
-                                    toUpdateData[properties[l]].push(resp[i][properties[l]]);
-                                }
-                            }
-                        }
+            var fieldsNotToUpdate = Object.keys(data.selectedChoices);
+            var toUpdateData = {};
+            // Organize data
+            var properties = Object.keys(resp);
+            for (var l = 0; l < properties.length; l++) {
+                if (!fieldsNotToUpdate.includes(properties[l])) {
+                    if (!toUpdateData.hasOwnProperty(properties[l])) {
+                        toUpdateData[properties[l]] = [];
                     }
+                    if (!toUpdateData[properties[l]].includes(resp[properties[l]]) && resp[properties[l]] !== null) {
+                        toUpdateData[properties[l]] = Object.values(resp[properties[l]]);
+                    }
+
                 }
-                // Update option
-                var toUpdateFields = Object.keys(toUpdateData);
-                for (var i = 0; i < elements.length; i++) {
-                    for (var l = 0; l < toUpdateFields.length; l++) {
-                        if (elements[i].id.replace('display_', '') === toUpdateFields[l]) {
-                            $(elements[i]).empty();
-                            var nullOption = document.createElement('option');
-                            nullOption.value = '';
-                            nullOption.text = '';
-                            elements[i].add(nullOption);
-                            for (var x = 0; x < toUpdateData[toUpdateFields[l]].length; x++) {
+            }
+
+            // Update option
+            var toUpdateFields = Object.keys(toUpdateData);
+            for (var i = 0; i < elements.length; i++) {
+                for (var l = 0; l < toUpdateFields.length; l++) {
+                    if (elements[i].id.replace('display_', '') === toUpdateFields[l]) {
+                        $(elements[i]).empty();
+                        var nullOption = document.createElement('option');
+                        nullOption.value = '';
+                        nullOption.text = '';
+                        elements[i].add(nullOption);
+                        for (var x = 0; x < toUpdateData[toUpdateFields[l]].length; x++) {
+                            if (toUpdateData[toUpdateFields[l]][x] !== null){
                                 var option = document.createElement('option');
                                 option.value = toUpdateData[toUpdateFields[l]][x];
                                 option.text = toUpdateData[toUpdateFields[l]][x];
