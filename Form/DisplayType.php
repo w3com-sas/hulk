3ca68b13050b2c5c14a856e6d9c5ca8406a59a3b
corentin-gillet <cgillet@w3com.fr>
2020-05-26 11:04:05 +0200

    h set filename value

--- Form/DisplayType.php
+++ Form/DisplayType.php
@@ -29,7 +29,10 @@ class DisplayType extends AbstractType
             $form = $formEvent->getForm();
 
             $filenameType = !empty($display->getDisplayNames()) ? ChoiceType::class : HiddenType::class;
-            $filenameOptions = !empty($display->getDisplayNames()) ? ['choices' => $display->getDisplayNames(), 'label' => 'Display'] : [];
+            $filenameOptions = !empty($display->getDisplayNames())
+                ? ['choices' => $display->getDisplayNames(), 'label' => 'Display']
+                : ['data' => $display->getDisplayName()];
+
             $form->add('filename', $filenameType, $filenameOptions);
 
             /** @var Property $globalSearchProperty */
